e011e905d66ff1db46fb6920e9b7eb0f309716b5
leonan123 <leonanorth3@gmail.com>
2023-10-18 23:07:48 -0300

    feat: add button, card and header component

---
+++ src/components/ui/header.tsx
@@ -0,0 +1,21 @@
+import { Button } from './button'
+import { MenuIcon, ShoppingCartIcon } from 'lucide-react'
+import { Card } from './card'
+
+export function Header() {
+  return (
+    <Card className="flex items-center justify-between p-[1.875rem]">
+      <Button size="icon" variant="outline">
+        <MenuIcon />
+      </Button>
+
+      <h1 className="text-lg font-semibold">
+        <span className="text-primary">FSW</span> Store
+      </h1>
+
+      <Button size="icon" variant="outline">
+        <ShoppingCartIcon />
+      </Button>
+    </Card>
+  )
+}
